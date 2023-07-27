8f48ebce095b8033424902bc0720754f71d09f7d
Sweta L K <sweta.k@ad.infosys.com>
2023-07-27 18:00:08 +0530

    build created

---
+++ build/precache-manifest.23a574209ccfde1ae08d2d2fcd946520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "170949171d6104335e19",
+    "url": "/static/css/main.7dfb875b.chunk.css"
+  },
+  {
+    "revision": "170949171d6104335e19",
+    "url": "/static/js/main.17094917.chunk.js"
+  },
+  {
+    "revision": "896316f0ad6c9720303b",
+    "url": "/static/js/1.896316f0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "128e356863150eb0debdeaa43645757d",
+    "url": "/index.html"
+  }
+];
