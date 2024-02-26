33161d0f5db9f5798f65c55ecbf94e56961f0a1a
Sweta L K <sweta.k@ad.infosys.com>
2024-02-26 12:41:51 +0530

    firebase listener added

--- src/Scanner/QuaggaBarcodeScanner/QuaggaBarcodeScanner.jsx
+++ src/Scanner/QuaggaBarcodeScanner/QuaggaBarcodeScanner.jsx
@@ -1,161 +1,44 @@
-import React, { useEffect, useRef, useState } from "react";
-import Quagga from "quagga";
-import "./QuaggaBarcodeScannerStyle.css";
+import React, { useEffect, useState } from "react";
+import db from "./firebase";
 
 const QuaggaBarcodeScanner = () => {
-  const firstUpdate = useRef(true);
-  const [isStart, setIsStart] = useState(false);
-  const [barcode, setBarcode] = useState("");
-
+  const [info, setInfo] = useState([]);
   useEffect(() => {
-    return () => {
-      if (isStart) stopScanner();
-    };
+    const unsubscribe = db
+      .collection("yourCollection")
+      .onSnapshot((snapshot) => {
+        const newData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setInfo(newData);
+      });
+    return unsubscribe;
   }, []);
-
-  useEffect(() => {
-    if (firstUpdate.current) {
-      firstUpdate.current = false;
-      return;
-    }
-
-    if (isStart) startScanner();
-    else stopScanner();
-  }, [isStart]);
-
-  const _onDetected = async (res) => {
-    setBarcode(res.codeResult.code);
-  };
-
-  const startScanner = () => {
-    Quagga.init(
-      {
-        inputStream: {
-          type: "LiveStream",
-          target: document.querySelector("#scanner-container"),
-          constraints: {
-            facingMode: "environment", // or user
-          },
-        },
-        numOfWorkers: navigator.hardwareConcurrency,
-        locate: true,
-        frequency: 1,
-        debug: {
-          drawBoundingBox: true,
-          showFrequency: true,
-          drawScanline: true,
-          showPattern: true,
-        },
-        multiple: false,
-        locator: {
-          halfSample: false,
-          patchSize: "large", // x-small, small, medium, large, x-large
-          debug: {
-            showCanvas: false,
-            showPatches: false,
-            showFoundPatches: false,
-            showSkeleton: false,
-            showLabels: false,
-            showPatchLabels: false,
-            showRemainingPatchLabels: false,
-            boxFromPatches: {
-              showTransformed: false,
-              showTransformedBox: false,
-              showBB: false,
-            },
-          },
-        },
-        decoder: {
-          readers: [
-            "code_128_reader",
-            "ean_reader",
-            "ean_8_reader",
-            "code_39_reader",
-            "code_39_vin_reader",
-            "codabar_reader",
-            "upc_reader",
-            "upc_e_reader",
-            "i2of5_reader",
-            "i2of5_reader",
-            "2of5_reader",
-            "code_93_reader",
-          ],
-        },
-      },
-      (err) => {
-        if (err) {
-          return console.log(err);
-        }
-        Quagga.start();
-      }
+  const Frame = ({ id }) => {
+    return (
+      <center>
+        <div className="div">
+          <p>Device id : {id}</p>
+        </div>
+      </center>
     );
-    Quagga.onDetected(_onDetected);
-    Quagga.onProcessed((result) => {
-      let drawingCtx = Quagga.canvas.ctx.overlay,
-        drawingCanvas = Quagga.canvas.dom.overlay;
-
-      if (result) {
-        if (result.boxes) {
-          drawingCtx.clearRect(
-            0,
-            0,
-            parseInt(drawingCanvas.getAttribute("width")),
-            parseInt(drawingCanvas.getAttribute("height"))
-          );
-          result.boxes
-            .filter((box) => box !== result.box)
-            .forEach((box) => {
-              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
-                color: "green",
-                lineWidth: 2,
-              });
-            });
-        }
-
-        if (result.box) {
-          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
-            color: "#00F",
-            lineWidth: 2,
-          });
-        }
-
-        if (result.codeResult && result.codeResult.code) {
-          Quagga.ImageDebug.drawPath(
-            result.line,
-            { x: "x", y: "y" },
-            drawingCtx,
-            { color: "red", lineWidth: 3 }
-          );
-        }
-      }
-    });
-  };
-
-  const stopScanner = () => {
-    Quagga.offProcessed();
-    Quagga.offDetected();
-    Quagga.stop();
   };
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      {/* <h3>Barcode scanner in React</h3>
-            <button
-                onClick={() => setIsStart((prevStart) => !prevStart)}
-                style={{ marginBottom: 20 }}
-            >
-                {isStart ? 'Stop' : 'Start'}
-            </button>
-            {isStart && (
-                <React.Fragment>
-                    <div id='scanner-container' />
-                    <span>Barcode: {barcode}</span>
-                </React.Fragment>
-            )} */}
-
       <a href="bledevices://bleconnect?userId=23423&shareId=234sdf">
         <button>URL Scheme</button>
       </a>
+      <div>
+        <center>
+          <h2>Device Details</h2>
+        </center>
+
+        {info.map((data) => (
+          <Frame course={data.CourseEnrolled} id={data.id} />
+        ))}
+      </div>
     </div>
   );
 };
